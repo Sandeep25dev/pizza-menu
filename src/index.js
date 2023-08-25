09ff16bac50c17ef8ea79b3a7fa461e1ab9dc138
Sandeep25Dev <sandeepkumar250903@gmail.com>
2023-08-26 02:23:35 +0530

    added some resource files

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return <h1>Hello People!</h1>;
+  return <h1>Hello Kings🤴🏻</h1>;
 }
 
+// React v-18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
